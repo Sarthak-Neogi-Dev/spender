1b9c1446aaf9d09c27e1059eea49809ce8650b39
Sarthak Neogi <sarthakneogi@gmail.com>
2026-07-25 23:39:00 +0530

    Revert "set up session routes for managing session data"
    This reverts commit aa44c5e0fa88a9875421c9791d522e34ae7dd457.

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,28 @@
 import express from "express";
 import connectDB from './lib/mongoose.ts';
-import router from './routes/session.ts';
+import Session from './models/sessions.model.ts';
+import type {ISession, IExpense, ITransfer} from './models/sessions.model.ts';
 
 const app = express();
 const port = 8080;
 
 connectDB();
 
-app.use(express.json());
-
-app.use('/session',router);
-
 app.get("/", (req, res)=>{
     res.send("Hello World");
 });
+
+app.get("/create",async (req, res)=>{
+    const S1 = new Session<ISession>({
+        members : ["a","b","c"],
+        totals : [0,0,0],
+        expenses : [],
+        transfers : [],
+        isOpen : true
+    });
+    await S1.save();
+    res.send(`Session Created : ${S1._id}`);
+})
 
 app.listen(port, ()=>{
     console.log(`Listenning at Port : ${port}`);
