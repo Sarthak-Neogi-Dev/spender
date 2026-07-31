28a4740125dcd772bb696678718efc0231d93348
Sarthak Neogi <sarthakneogi@gmail.com>
2026-07-31 23:04:34 +0530

    Add styling to home page

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -2,9 +2,9 @@ import {Link} from "react-router";
 
 export default function Home() {
   return <>
-    <div>
-      <div><Link to="/create">Create Session</Link></div>
-      <div><Link to="/join">Join Session</Link></div>
+    <div className="button_box">
+      <div className="button"><Link to="/create">Create Session</Link></div>
+      <div className="button"><Link to="/join">Join Session</Link></div>
     </div>
   </>;
 }
