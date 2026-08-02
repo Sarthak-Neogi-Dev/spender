2bd8b9c0b81bcc071ac7bbfa33977646cc117c38
Sarthak Neogi <sarthakneogi@gmail.com>
2026-08-02 23:51:39 +0530

    added styling to close page

--- frontend/app/routes/close.tsx
+++ frontend/app/routes/close.tsx
@@ -4,7 +4,7 @@ import type { Iclose } from "../stores/close.store";
 export default function Close() {
     const { close } = useClose() as Iclose;
     return <>
-        <div>
+        <div className="closing_screen">
             <h1>This Session is Closed.</h1>
             {close.map((item: string, idx: number) => (<p key={idx}>{item}</p>))}
         </div>
