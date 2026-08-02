e01550a1527d6235b9e1f51482674fa519a63f86
Sarthak Neogi <sarthakneogi@gmail.com>
2026-08-02 15:22:47 +0530

    add fetching system to main page

--- frontend/app/routes/main.tsx
+++ frontend/app/routes/main.tsx
@@ -1,10 +1,36 @@
+import axios from "axios";
 import useId from "../stores/id.store";
-import type {IuseId} from "../stores/id.store";
+import type { IuseId } from "../stores/id.store";
+import { useEffect, useState } from "react";
 
-export default function Main(){
-    const {id} = useId() as IuseId;
+export default function Main() {
+    const [data, setData] = useState<any>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isError, setIsError] = useState<boolean>(false);
+    const { id } = useId() as IuseId;
+
+    async function fetchSession(id: string) {
+        setIsLoading(true);
+        const res = await axios.get(`http://localhost:8080/session/${id}`);
+        return res.data;
+    }
+
+    useEffect(() => {
+        if (id) {
+            fetchSession(id).then(data => {
+                setData(data);
+            }).catch(err => {
+                setIsError(true);
+            }).finally(() => {
+                setIsLoading(false);
+            });
+        }
+        else {
+            setIsError(true);
+        }
+    }, [])
 
     return <>
-        <h1>You have joined Session : {id}</h1>
+        {isError ? <h1>Something Went Wrong</h1> : isLoading ? <h1>Loading...</h1> : <>{data}</>}
     </>
 }
