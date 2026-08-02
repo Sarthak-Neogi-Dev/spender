82d470a5c3b9741ad19c57b60e6421e014d3912a
Sarthak Neogi <sarthakneogi@gmail.com>
2026-08-02 17:42:27 +0530

    display expenses

--- frontend/app/routes/main.tsx
+++ frontend/app/routes/main.tsx
@@ -7,6 +7,7 @@ export default function Main() {
     const [data, setData] = useState<any>(null);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [isError, setIsError] = useState<boolean>(false);
+    const [trigger, setTrigger] = useState(0);
     const { id } = useId() as IuseId;
 
     async function fetchSession(id: string) {
@@ -14,6 +15,15 @@ export default function Main() {
         const res = await axios.get(`http://localhost:8080/session/${id}`);
         const data = JSON.parse(res.data);
         return data;
+    }
+
+    async function deleteExpense(expense_id: number) {
+        const data = {
+            "expense_id": expense_id,
+            "id": id
+        }
+        await axios.post(`http://localhost:8080/session/delete/`, data);
+        setTrigger(trigger + 1);
     }
 
     useEffect(() => {
@@ -29,13 +39,28 @@ export default function Main() {
         else {
             setIsError(true);
         }
-    }, [])
+    }, [trigger])
 
     return <>
         {isError ? <h1>Something Went Wrong</h1> : isLoading ? <h1>Loading...</h1> : <>
-            {data.members.map((member: string, idx: number) => (
-                <div>{member} : {data.totals[idx]}</div>
-            ))}
+
+            <div>
+                <h1>Members :</h1>
+                {data.members.map((member: string, idx: number) => (
+                    <div key={idx}>{member} : {data.totals[idx]}</div>
+                ))}
+            </div>
+
+            <div>
+                <h1>Expenses :</h1>
+                {data.expenses.map((expense: { payer_id: number, title: string, amount: number }, idx: number) => (
+                    <div key={idx}>
+                        <p>{data.members[expense.payer_id]} : {expense.title} : {expense.amount}</p>
+                        <button onClick={() => { deleteExpense(idx) }}>Delete</button>
+                    </div>
+                ))}
+            </div>
+
         </>}
     </>
 }
